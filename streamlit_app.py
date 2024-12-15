791196d4037b5eafa99c7e4e2bf51830a7d21b90
hayun72 <20242217@sdhs.gwe.hs.kr>
2024-12-15 13:02:16 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ def display_left_content(image, prediction, probs, labels):
     st.markdown("<h4>클래스별 확률:</h4>", unsafe_allow_html=True)
     for label, prob in zip(labels, probs):
         st.markdown(f"""
-            <div style="background-color: #FBEFFB; border-radius: 5px; padding: 5px; margin: 5px 0;">
+            <div style="background-color: #F3F2F2; border-radius: 5px; padding: 5px; margin: 5px 0;">
                 <strong style="color: #333;">{label}:</strong>
                 <div style="background-color: #FBEFFB; border-radius: 5px; width: 100%; padding: 2px;">
                     <div style="background-color: #F781BE; width: {prob*100}%; padding: 5px 0; border-radius: 5px; text-align: center; color: white;">
